e07e94287bcc41966c202182389b79ad0753bbc3
Maro <maro.huang@hukaka.com>
2021-03-12 16:32:12 +0800

    唉

--- Day12/Day12.py
+++ Day12/Day12.py
@@ -1,27 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## 作業目標：搭建一個bag of words模型
-
-# ---
-
-# In[7]:
-
-
 import pandas as pd
 import nltk
-
-nltk.download('punkt')
-# nltk.download()
+#nltk.download()
 import numpy as np
 
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
 corpus = dataset['Review'].values
-
-# ### 從文本中取出所有單字
-
-# In[7]:
-
 
 whole_words = []
 for sentence in corpus:
@@ -29,57 +12,30 @@ for sentence in corpus:
     for word in tokenized_sentence:
         whole_words.append(word)
 
-# ### 移除重複單字
 whole_words = set(whole_words)
-
-# In[12]:
-
-
 print('共有{}個單字'.format(len(whole_words)))
-
-# ### 建立字典使每一個單字有對應數值
-
-# In[18]:
-
 
 word_index = {}
 index_word = {}
 n = 0
 for word in whole_words:
-    """自行填入"""
-    print(word_index(word))
-# In[10]:
-
+    word_index[word] = n
+    index_word[n] = word
+    n+=1
 
 print(word_index)
-
-# In[11]:
-
-
 print(index_word)
 
 
-# ## 轉換句子為bag of words型式
-
-# In[34]:
-
-
-# def _get_bag_of_words_vector(sentence, word_index_dic, whole_words):
-#     sentence = sentence
-#     '''創建一個vector'''
-#     for word in nltk.word_tokenize(sentence):
-#         if word in whole_words:
-#             '''自行填入'''
-#     return vector
+def _get_bag_of_words_vector(sentence, word_index_dic, whole_words):
+    sentence = sentence
+    vector = np.zeros(len(whole_words))
+    for word in nltk.word_tokenize(sentence):
+        if word in whole_words:
+            vector[word_index[word]]+=1
+    return vector
 
 
-# In[35]:
-
-
-# _get_bag_of_words_vector('Wow... Loved this place.', word_index, whole_words)
-
-# In[ ]:
-
-
-
+vector = _get_bag_of_words_vector('Wow... Loved this place.', word_index, whole_words)
+print(vector)
 
